96e3cef4df9e126385eeb1078a8b7b9b803ad851
Dave86dev <davidochando1986@gmail.com>
2024-02-10 13:19:01 +0100

    feat: key issue solved

--- src/common/JsonRender/JsonRender.tsx
+++ src/common/JsonRender/JsonRender.tsx
@@ -5,26 +5,28 @@ import { StackElement } from "../../interfaces";
 export const JsonRender: React.FC = ({}) => {
   //Here we go .. let's rock with the iterative function
 
-  const renderIteration = (json: unknown) => {
+  const renderIteration = (json: unknown, parentPath = '') => {
 
     //let's see if you are a JSON and you are not null....
     if (typeof json === "object" && json !== null) {
 
-      const stack: StackElement[] = Object.entries(json).map(([key, value]) => ({ key, value }));
+      const stack: (StackElement & {parentPath: string})[] = Object.entries(json).map(([key, value]) => ({ key, value, parentPath }));
       const elements: JSX.Element[] = [];
 
       while(stack.length > 0){
         //careful with this in the future!, need to handle error....
-        const { key, value } = stack.pop()!;
+        const { key, value, parentPath } = stack.pop()!;
+        const currentPath = `${parentPath}.${key}`;
 
         if (typeof value === 'object' && value !== null) {
             Object.entries(value).forEach(([childKey, childValue]) => {
-              stack.push({ key: childKey, value: childValue });
+              stack.push({ key: childKey, value: childValue, parentPath: currentPath });
             });
-            elements.push(<div key={key}><strong>{key}:</strong></div>);
+            elements.push(<div key={currentPath}><strong>{key}:</strong></div>);
           } else {
             // Handle non-object values
-            elements.push(<div key={key}>{key}: {String(value)}</div>);
+            console.log(value)
+            elements.push(<div key={currentPath}>{key}: {String(value)}</div>);
           }
       }
 
