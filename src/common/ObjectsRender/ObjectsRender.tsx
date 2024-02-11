f31598a6d519e5d8949fc47a1f2abe44ca6b679a
Dave86dev <davidochando1986@gmail.com>
2024-02-11 01:16:13 +0100

    feat: nested almost done, need to correct objects margin

--- src/common/ObjectsRender/ObjectsRender.tsx
+++ src/common/ObjectsRender/ObjectsRender.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import "./ObjectsRender.css";
 import { ObjectsComponentProps } from '../../interfaces';
+import { ArraysRender } from '../ArraysRender/ArraysRender'; 
+import { PrimitivesRender } from '../PrimitivesRender/PrimitivesRender';
 
-export const ObjectsRender: React.FC<ObjectsComponentProps> = ({ keyName, value }) => {
-  const keysCount = Object.keys(value).length;
+export const ObjectsRender: React.FC<ObjectsComponentProps> = ({
+  value,
+  depth = 0,
+}) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpand = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    event.stopPropagation(); 
+    setIsExpanded(!isExpanded);
+  };
 
   return (
-    <div>
-      <strong>{keyName}:</strong> Object containing {keysCount} properties.
+    <div className='keyObject' style={{ marginLeft: `${depth * 0.8}em`, cursor: 'pointer' }} onClick={toggleExpand}>
+      {'{}'} {isExpanded ? '[-]' : '[+]'}
+      {isExpanded && (
+        <div>
+          {Object.entries(value).map(([nestedKey, nestedValue]) => {
+            const isObject = nestedValue !== null && typeof nestedValue === 'object' && !Array.isArray(nestedValue);
+            const isArray = Array.isArray(nestedValue);
+            return (
+              <div key={nestedKey} style={{ marginLeft: `${(depth + 1) * 0.8}em` }}>
+                {isArray ? (
+                  <ArraysRender keyName={nestedKey} value={nestedValue} depth={depth + 1} />
+                ) : isObject ? (
+                  <ObjectsRender value={nestedValue} depth={depth + 1} />
+                ) : (
+                  <PrimitivesRender keyName={nestedKey} value={nestedValue} />
+                )}
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
